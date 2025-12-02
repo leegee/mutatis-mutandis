9912190587519a4854e0b0921a42d8b911de59fa
Lee Goddard <leegee@gmail.com>
2025-12-02 01:04:30 +0100

    nams

--- docs/run-dot.sh
+++ docs/run-dot.sh
@@ -4,30 +4,30 @@ OUTFILE="${1:-pamphlet_pipeline.$TYPE}"
 
 
 # https://graphviz.org/doc/info/colors.html#brewer
-export COLORSCHEME="gnbu3"
+export COLORSCHEME="gnbu4"
 
 # Set hierarchical colours (1 = lightest/top-level, 2 = subcluster, 3 = nodes)
-export CLUSTER_FILL="1"
-export SUBCLUSTER_FILL="2"
-export NODE_FILL="3"
+export CLUSTER_BG="1"
+export SUBCLUSTER_BG="2"
+export NODE_BG="3"
 
-export CLUSTER_FONT="#333333"
-export SUBCLLUSTER_FONT="#333333"
-export NODE_FONT="#333333"
+export CLUSTER_FG="#333333"
+export SUBCLUSTER_FG="#333333"
+export NODE_FG="#333333"
 
 # Read template and substitute variables
 sed \
-  -e "s/\${CLUSTER_FILL}/$CLUSTER_FILL/g" \
-  -e "s/\${SUBCLUSTER_FILL}/$SUBCLUSTER_FILL/g" \
-  -e "s/\${NODE_FILL}/$NODE_FILL/g" \
-  -e "s/\${CLUSTER_FILL}/$CLUSTER_FONT/g" \
-  -e "s/\${SUBCLUSTER_FILL}/$SUBCLUSTER_FONT/g" \
-  -e "s/\${NODE_FONT}/$NODE_FONT/g" \
+  -e "s/\${CLUSTER_BG}/$CLUSTER_BG/g" \
+  -e "s/\${SUBCLUSTER_BG}/$SUBCLUSTER_BG/g" \
+  -e "s/\${NODE_BG}/$NODE_BG/g" \
+  -e "s/\${CLUSTER_FG}/$CLUSTER_FG/g" \
+  -e "s/\${SUBCLUSTER_FG}/$SUBCLUSTER_FG/g" \
+  -e "s/\${NODE_FG}/$NODE_FG/g" \
   -e "s/\${COLORSCHEME}/$COLORSCHEME/g" \
   pamphlet_pipeline.dot.template > /tmp/pamphlet_pipeline.dot
 
-dot -T${TYPE} /tmp/pamphlet_pipeline.dot -o "$OUTFILE"
+dot -Gengine=neato -T${TYPE} /tmp/pamphlet_pipeline.dot -o "$OUTFILE"
 
-cat /tmp/pamphlet_pipeline.dot
+# cat /tmp/pamphlet_pipeline.dot
 
 echo "Graph generated -> $OUTFILE"
