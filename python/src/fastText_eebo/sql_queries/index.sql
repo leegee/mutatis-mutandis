d553c5b5c5b66d9635c6efe7b908d1f1d52ad607
Lee Goddard <leegee@gmail.com>
2026-01-09 09:19:21 +0100

    Util

---
+++ python/src/fastText_eebo/sql_queries/index.sql
@@ -0,0 +1,11 @@
+-- index.sql
+
+-- .mode table | column | csv | json | markdown
+.mode markdown
+-- .mode csv
+.width 5 5 20 20 2 20
+.nullvalue NULL
+.headers on
+
+-- .read ./liberty_1641.sql
+.read ./liberty_all.sql
