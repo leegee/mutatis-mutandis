1a8ac5bf3a2ecf028570e136fc987eac36d47ce1
Lee Goddard <leegee@gmail.com>
2026-04-09 21:52:17 +0200

    Reverse proxy out/ as api/

--- gui/eebo-frontend/vite.config.ts
+++ gui/eebo-frontend/vite.config.ts
@@ -1,6 +1,23 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import path from "path";
+import { serveOutJsonPlugin } from "./vite-custom-plugins/serveOutJsonPlugin";
+
+const documentRoot = path.resolve(__dirname, "../../out");
+
+console.info(`[vite.config] documentRoot: ${documentRoot}`);
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [
+    solid(),
+    serveOutJsonPlugin(documentRoot),
+  ],
+  server: {
+    fs: {
+      allow: [
+        path.resolve(__dirname),           // frontend root
+        path.resolve(__dirname, "../out"), // JSON folder
+      ],
+    },
+  },
+});
