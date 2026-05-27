53c79c00418892d21b4335dbcc6371d54e5b207a
Lee Goddard <leegee@gmail.com>
2026-05-27 11:51:57 +0200

    menu; lazy load

--- gui/eebo-frontend/src/App.tsx
+++ gui/eebo-frontend/src/App.tsx
@@ -1,31 +1,59 @@
 // src/App.tsx
 
-import { createResource, createSignal, ErrorBoundary, Match, Show, Switch } from "solid-js";
+import {
+  createResource,
+  createSignal,
+  ErrorBoundary,
+  lazy,
+  Match,
+  Show,
+  Switch,
+} from "solid-js";
 import { Transition } from "solid-transition-group";
 import ConceptGraphGuide from "./components/ConceptGraph/ConceptGraphGuide";
 import { loadConceptNeighbours } from "./components/ConceptGraph/loadConceptNeighbours.service";
-// import ConceptGraph from "./components/ConceptGraph/ConceptGraph";
-// import ConceptGraph from "./components/ConceptGraph2";
-// import ConceptGraph from "./components/ConceptGraph3";
-// import NeighbourhoodBrowser from "./components/NeighbourhoodBrowser";
-// import ContextGraph4, { type Tier2Data } from "./components/ContextGraph4";
-import NeighbourhoodBrowser from "./components/NeighbourhoodBrowser";
-import ContextGraph5 from "./components/ContextGraph5";
 
-import "./App.css"
-import DiachronicChart from "./components/DiachronicChart";
+const ContextGraph5 = lazy(() => import("./components/ContextGraph5"));
+const NeighbourhoodBrowser = lazy(
+  () => import("./components/NeighbourhoodBrowser")
+);
+const DiachronicChart = lazy(
+  () => import("./components/DiachronicChart")
+);
+const CosmosContextGraph = lazy(
+  () => import("./components/CosmosContextGraph")
+);
+
+import "./App.css";
 
 export default function App() {
   const [events] = createResource(loadConceptNeighbours);
   const [openHelp, setOpenHelp] = createSignal(false);
   const [open, setOpen] = createSignal(false);
-  const [view, setView] = createSignal<'graph' | 'table' | 'help' | 'diachronic'>('graph')
+
+  const [view, setView] = createSignal<
+    "graph" | "table" | "help" | "diachronic" | "cosmos"
+  >("graph");
+
+  const navItems = [
+    { key: "graph", icon: "graph_5", label: "Event FDG" },
+    { key: "table", icon: "view_column", label: "Neighbourhood Table" },
+    { key: "diachronic", icon: "hourglass", label: "Diachronic Chart" },
+    { key: "cosmos", icon: "experiment", label: "Cosmos FDG" },
+  ] as const;
 
   return (
     <>
-      <nav id='app-nav' class={`surface-container fill left scroll ${ open() ? 'max' : 'small' }`}>
+      <nav
+        id="app-nav"
+        class={`surface-container fill left scroll ${ open() ? "max" : "small"
+          }`}
+      >
         <header class="center-align ">
-          <button class="extra transparent" onClick={() => setOpen(!open())}>
+          <button
+            class="extra transparent"
+            onClick={() => setOpen(!open())}
+          >
             <Switch>
               <Match when={open()}>
                 <i>menu_open</i>
@@ -37,65 +65,73 @@ export default function App() {
           </button>
         </header>
 
-        <a onClick={() => setView('graph')}>
-          <i>graph_5</i>
-          <span>Event Graph</span>
-        </a>
-        <a onClick={() => setView('table')}>
-          <i>view_column</i>
-          <span>Neighbourhood Table</span>
-        </a>
-        <a onClick={() => setView('diachronic')}>
-          {/* <i>experiment</i> */}
-          <i>hourglass</i>
-          <span>Diachronic Chart</span>
-        </a>
+        {navItems.map((item) => (
+          <a
+            onClick={() => setView(item.key)}
+            classList={{
+              active: view() === item.key,
+            }}
+          >
+            <i>{item.icon}</i>
+            <span>{item.label}</span>
+          </a>
+        ))}
 
         <hr />
 
-        <a class="" onClick={() => setOpenHelp(!openHelp())}>
+        <a onClick={() => setOpenHelp(!openHelp())}>
           <i>help</i>
           <span>Guide</span>
         </a>
       </nav>
 
       <main class="responsive max no-padding">
-        <ErrorBoundary fallback={
-          (err) => <article>
-            <div class="error padding">{err.message}</div>
-          </article>
-        }>
-          <Show when={events()} fallback={
-            <article class="small-round padding border medium no-padding">
-              <div class="padding absolute center middle">
-                <h5>Loading events...</h5>
-              </div>
-              <progress />
+        <ErrorBoundary
+          fallback={(err) => (
+            <article>
+              <div class="error padding">{err.message}</div>
             </article>
-          }>
+          )}
+        >
+          <Show
+            when={events()}
+            fallback={
+              <article class="small-round padding border medium no-padding">
+                <div class="padding absolute center middle">
+                  <h5>Loading events...</h5>
+                </div>
+                <progress />
+              </article>
+            }
+          >
             {(data) => (
               <Switch>
-                <Match when={view() === 'graph'}>
-                  {/* <ContextGraph4 data={data()} /> */}
+                <Match when={view() === "graph"}>
                   <ContextGraph5 data={data()} />
                 </Match>
-                <Match when={view() === 'table'}>
+
+                <Match when={view() === "table"}>
                   <NeighbourhoodBrowser data={data()} />
                 </Match>
-                <Match when={view() === 'diachronic'}>
+
+                <Match when={view() === "diachronic"}>
                   <DiachronicChart data={data()} />
+                </Match>
+
+                <Match when={view() === "cosmos"}>
+                  <CosmosContextGraph data={data()} />
                 </Match>
               </Switch>
             )}
           </Show>
         </ErrorBoundary>
-      </main >
+      </main>
 
       <Transition name="slide-fade">
         <Show when={openHelp()}>
           <article class="helpContainer right" style="width: 32rem">
             <Switch fallback={<article>To do...</article>}>
-              <Match when={view() === 'graph'}>
+              <Match when={view() === "graph"}>
                 <ConceptGraphGuide />
               </Match>
             </Switch>
