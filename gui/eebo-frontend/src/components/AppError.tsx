f54b37b97f0e2cdb8e8666f7c8372c3af95cc6e0
Lee Goddard <leegee@gmail.com>
2026-06-03 11:34:32 +0200

    catch other errors

--- gui/eebo-frontend/src/components/AppError.tsx
+++ gui/eebo-frontend/src/components/AppError.tsx
@@ -4,7 +4,9 @@ interface Props {
 }
 
 export default function AppError(props: Props) {
-  const isChunkError = props.err.message.startsWith(
+  const message = props.err?.message ?? String(props.err);
+
+  const isChunkError = message.startsWith(
     "Failed to fetch dynamically imported module",
   );
 
@@ -12,11 +14,13 @@ export default function AppError(props: Props) {
     <article class="small-round padding border medium no-padding">
       <div class="padding error absolute center middle">
         <h3>Error</h3>
+
         {isChunkError ? (
           <p>App update required. Please refresh.</p>
         ) : (
-          <p>Something went wrong: {props.err.message}</p>
+          <pre style="background: transparent; color: black">{message}</pre>
         )}
+
         <nav>
           <button class="error border" onClick={props.reset}>
             <i>restart_alt</i>
