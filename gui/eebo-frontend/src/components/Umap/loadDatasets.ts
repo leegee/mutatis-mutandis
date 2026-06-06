bdf28060eb2602311b99708ef2179b14d8ba6521
Lee Goddard <leegee@gmail.com>
2026-06-06 12:35:02 +0200

    augment with event meta

--- gui/eebo-frontend/src/components/Umap/loadDatasets.ts
+++ gui/eebo-frontend/src/components/Umap/loadDatasets.ts
@@ -1,5 +1,4 @@
 import { getEventsByIds } from "../../services/db/getEventsByIds";
-import type { Event } from "../../types";
 import type { ConceptDataset } from "./types";
 
 export async function loadDatasets() {
@@ -11,16 +10,36 @@ export async function loadDatasets() {
     // Augment each point with its concept name so colorBy:"concept" works.
     const augmented = (d: ConceptDataset) => ({
         ...d,
-        points: d.points.map(
-            (p) => ({ ...p, concept: d.concept })),
+        points: d.points.map((p) => ({
+            ...p,
+            concept: d.concept
+        }))
     });
 
     const datasetsTagged = [a, b].map(augmented);
+    const allEventIds = [
+        ...new Set(datasetsTagged.flatMap(d => d.points.map(p => p.event_id)))
+    ];
 
-    const eids = datasetsTagged.map(_ => _.points.map(_ => _.event_id));
+    const events = await getEventsByIds(allEventIds);
 
-    const events = await getEventsByIds(eids[0]);
-    console.log("EVENTS-------------------\n", events);
+    // Turn BigInt ids into strings
+    const eventMap = new Map(
+        events.map(e => [String(e.event_id), e])
+    );
 
-    return datasetsTagged;
+    // merge event fields into points
+    const enrichedDatasets = datasetsTagged.map(d => ({
+        ...d,
+        points: d.points.map(p => {
+            // Turn BigInt ids into strings
+            const event = eventMap.get(String(p.event_id));
+            return {
+                ...p,
+                ...(event ?? { whoops: true })
+            };
+        })
+    }));
+
+    return enrichedDatasets;
 }
