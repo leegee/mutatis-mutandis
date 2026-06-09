19d42b20a36d5192bb1d144a0d9f0b0001207fd3
Lee Goddard <leegee@gmail.com>
2026-06-09 18:52:37 +0200

    extend range

--- gui/eebo-frontend/src/corpus_config.ts
+++ gui/eebo-frontend/src/corpus_config.ts
@@ -2,5 +2,5 @@
 export const CORPUS_TIER2_DB_URL = "/data/tier2_concept_neighbours.db";
 
 export const CORPUS_START_YEAR = 1625;
-export const CORPUS_END_YEAR = 1665;
+export const CORPUS_END_YEAR = 1689;
 
