1b1bf8437718866c36de63762448aabb9f2a1001
Lee Goddard <leegee@gmail.com>
2026-07-13 10:37:02 +0200

    Update triggers on both layers

--- gui/eebo-frontend/src/components/ScatterPlot/GlowScatterplotLayer.tsx
+++ gui/eebo-frontend/src/components/ScatterPlot/GlowScatterplotLayer.tsx
@@ -117,6 +117,7 @@ export class GlowScatterplotLayer<DataT extends {} = any>
         getFillColor: colorAccessor,
         radiusUnits,
         transitions,
+        updateTriggers,
         opacity,
         ...coreProps,
       }),
