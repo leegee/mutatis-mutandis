decf215cb3de4f8f5fe21fce45a8e76bd6e3c14e
Lee Goddard <leegee@gmail.com>
2026-07-23 13:46:38 +0200

    Extrapolate settings

---
+++ gui/eebo-frontend/src/components/ScatterPlot/PlotSettings.tsx
@@ -0,0 +1,37 @@
+import { controlsActions } from "../../state/controls.actions";
+import { controls } from "../../state/controls.store";
+
+export default function PlotSettings() {
+    return <>
+        <li class="middle-align top-padding">
+            <div class="field middle-align prefix suffix">
+                <nav>
+                    <div class="slider medium responsive">
+                        <input type='range' min={0} max={4} step={0.5}
+                            value={controls.bfsOpacity}
+                            onInput={(e) => controlsActions.setBfsOpacity(e.currentTarget.value)}
+                        />
+                        <span><i>brightness_6</i></span>
+                    </div>
+                </nav>
+                <output>BFS Background Opacity</output>
+            </div>
+        </li>
+
+        <li class="middle-align top-padding">
+            <div class="field middle-align prefix suffix">
+                <nav>
+                    <div class="slider medium responsive">
+                        <input type='range' min={0} max={255} step={5}
+                            disabled={!controls.showNeighbours}
+                            value={controls.neighbourOpacity}
+                            onInput={(e) => controlsActions.setNeighbourOpacity(Number(e.currentTarget.value))}
+                        />
+                        <span><i>brightness_6</i></span>
+                    </div>
+                </nav>
+                <output>Neighbour Opacity</output>
+            </div>
+        </li>
+    </>
+}
--- gui/eebo-frontend/src/components/ScatterPlot/index.tsx
+++ gui/eebo-frontend/src/components/ScatterPlot/index.tsx
@@ -15,6 +15,7 @@ import ClusterTooltip from "./ClusterTooltip";
 import ConceptTooltip from "./ConceptTooltip";
 import Plot from "./Plot";
 import { loadDatasets, loadBfsDataset } from "./loadScatterDatasets.sqlite";
+import PlotSettings from "./PlotSettings";
 
 const COLOR_FIELDS = ["doc_id", "pub_year", "concept", "cluster_id"];
 
@@ -173,43 +174,12 @@ export default function ConceptClusterPlot() {
 
                         <div class="no-round bottom">
                             <button class="transparent circle">
-                                <i>more_vert</i>
+                                <i>settings</i>
                             </button>
                             <menu class="no-round  bottom left no-wrap">
-
-                                <li class="middle-align top-padding">
-                                    <div class="field middle-align prefix suffix">
-                                        <nav>
-                                            <div class="slider medium responsive">
-                                                <input type='range' min={0} max={4} step={0.5}
-                                                    value={controls.bfsOpacity}
-                                                    onInput={(e) => controlsActions.setBfsOpacity(e.currentTarget.value)}
-                                                />
-                                                <span><i>brightness_6</i></span>
-                                            </div>
-                                        </nav>
-                                        <output>BFS Background Opacity</output>
-                                    </div>
-                                </li>
-
-                                <li class="middle-align top-padding">
-                                    <div class="field middle-align prefix suffix">
-                                        <nav>
-                                            <div class="slider medium responsive">
-                                                <input type='range' min={0} max={255} step={5}
-                                                    disabled={!controls.showNeighbours}
-                                                    value={controls.neighbourOpacity}
-                                                    onInput={(e) => controlsActions.setNeighbourOpacity(Number(e.currentTarget.value))}
-                                                />
-                                                <span><i>brightness_6</i></span>
-                                            </div>
-                                        </nav>
-                                        <output>Neighbour Opacity</output>
-                                    </div>
-                                </li>
-
+                                <PlotSettings />
                             </menu>
-                            <span class="tooltip right">More...</span>
+                            <span class="tooltip bottom">Plot Settings</span>
                         </div>
                     </ControlsHeader>
 
