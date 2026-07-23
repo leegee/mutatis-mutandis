7e6621850fa176dd4dfd3e56b6bd707fe669cb1e
Lee Goddard <leegee@gmail.com>
2026-07-23 14:39:28 +0200

    Tidy

--- gui/eebo-frontend/src/components/ScatterPlot/ConceptTooltip.tsx
+++ gui/eebo-frontend/src/components/ScatterPlot/ConceptTooltip.tsx
@@ -15,38 +15,36 @@ export default function ConceptTooltip(props: Props) {
                 <h2 class="fill max">
                     <q>{point().token}</q>
                 </h2>
-
-                <Show when={point().depth}>
-                    <div class="medium-opacity small-text no-space small small-margin tiny-padding">
-                        <span class="max no-space small small-margin no-padding">
-                            <span class="bold">{point().pub_year}</span>{" "}
-                            {point().concept}
-                            <sup class="medium-text">
-                                {" "}{point().depth}
-                            </sup>
-                        </span>
-                    </div>
-                </Show>
             </header>
 
+
+            <div class="left-padding right-padding bottom-margin">
+                <span class="large-opacity">
+                    <span class="bold">
+                        {point().pub_year}
+                    </span>
+                    <span class="large-padding">&mdash; </span>
+                    {point().concept}
+                </span>
+            </div>
+
             <div class="left-padding right-padding">
                 <span class="medium-opacity">
-                    Doc: {point().doc_id} T {point().token_idx}
+                    Document ID: {point().doc_id}
                     <br />
-                    Win: {point().window_id} T {point().window_token_pos}
+                    Token {point().token_idx}
+                    <br />
+                    Window: {point().window_id}
                 </span>
             </div>
 
             <div class="left-padding right-padding">
                 <span class="medium-opacity">
-                    Cluster {point().cluster_id || "N/A"}
+                    Cluster {point().cluster_id || "None"}
                 </span>
             </div>
 
-            <footer
-                class="row padding fill"
-                style="bottom-padding: 1em; top-padding: 1em"
-            >
+            <footer class="row padding fill bottom-padding top-padding" >
                 <TextWindow
                     eventid={point().event_id}
                     style="font-size: 12pt; line-height: 1.6;"
