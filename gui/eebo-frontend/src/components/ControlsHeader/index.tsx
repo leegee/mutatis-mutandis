b6caf95eae3d0891ca9f4d03a555ee9db3f1ddd9
Lee Goddard <leegee@gmail.com>
2026-07-23 13:43:01 +0200

    Tidy

--- gui/eebo-frontend/src/components/ControlsHeader/index.tsx
+++ gui/eebo-frontend/src/components/ControlsHeader/index.tsx
@@ -121,18 +121,19 @@ const ControlsHeader: ParentComponent<Props> = (props) => {
           </div>
         </Show>
 
+        <Show when={props.authorMatch}>
+          <nav class="toolbar no-round no-margin no-padding">
+            <div class="field label border small no-margin no-padding" >
+              <input type='search' value={controls.authorMatch || ''} onChange={(e) => A.setAuthorMatch(e.currentTarget.value)} />
+              <label>Match Author</label>
+              <span class="tooltip bottom">Match authors containing characters entered here</span>
+            </div>
+          </nav>
+        </Show>
+
         {resolved()}
       </nav>
 
-      <Show when={props.authorMatch}>
-        <nav class="toolbar no-round no-margin no-padding">
-          <div class="field label border small no-margin no-padding" >
-            <input type='search' value={controls.authorMatch || ''} onChange={(e) => A.setAuthorMatch(e.currentTarget.value)} />
-            <label>Match Author</label>
-            <span class="tooltip bottom">Match authors containing characters entered here</span>
-          </div>
-        </nav>
-      </Show>
     </>
   );
 };
